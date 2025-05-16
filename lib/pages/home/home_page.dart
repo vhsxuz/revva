81cd13891679b06fe4ae03c102cab8c64b77fbe1
sahidjafar <sahid11.sj@gmail.com>
2025-05-16 21:33:04 +0700

    add: layout appBar and bottomNav

---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:revva/config/component/appBar/main_app_bar.dart';
+import 'package:revva/config/component/bottomNav/main_bottom_nav.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int selectedIndex = 0;
+
+  void onNavItemSelected(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0XFFF4F4F4),
+      appBar: const MainAppBar(),
+      body: Stack(children: [MainBottomNav()]),
+    );
+  }
+}
