8d78cbf86d06f7c378fa974761b9935ff4144ed9
sahidjafar <sahid11.sj@gmail.com>
2025-05-19 22:26:52 +0700

    add: CRUD Employee with Get X

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
       title: 'Revva',
       debugShowCheckedModeBanner: false,
       // Essential routing configurations:
-      initialRoute: Routes.EMPLOYEE,
+      initialRoute: Routes.ONBOARDING,
       getPages: AppPages.routes,
       unknownRoute: GetPage(
         name: '/notfound',
